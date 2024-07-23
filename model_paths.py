022d15009a5e3653dbe059499aa30ec3e774c068
Marzie Ghorbani <60660630+katrina-coder@users.noreply.github.com>
2024-07-24 03:23:47 +1000

    Update model_paths.py

--- model_paths.py
+++ model_paths.py
@@ -15,7 +15,7 @@ else:
 
 warnings.filterwarnings('ignore')
 
-models = {"Current density": joblib.load(f"{model_dir}/RF_log icorr"),
+models = {"Current density": joblib.load(f"{model_dir}/RF_icorr"),
           "Corrosion potential": joblib.load(f"{model_dir}/RF_Ecorr"),
           "Pitting potential": joblib.load(f"{model_dir}/RF_Pcorr")
           }
