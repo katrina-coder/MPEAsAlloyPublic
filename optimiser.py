bab96df3e923a40162b8e05d783fc83c1fb979b2
Marzie Ghorbani <60660630+katrina-coder@users.noreply.github.com>
2024-07-26 13:54:15 +1000

    Update optimiser.py

--- optimiser.py
+++ optimiser.py
@@ -6,6 +6,17 @@ import pickle
 #     from MPEAsAlloyPublic.model_paths import models
 # else:
 #     from model_paths import models
+import joblib
+import pickle
+import warnings
+warnings.filterwarnings('ignore')
+
+model_dir = "/content/drive/MyDrive/MPEAs"
+
+models = {"Current density": joblib.load(f"{model_dir}/RF_icorr"),
+          "Corrosion potential": joblib.load(f"{model_dir}/RF_Ecorr"),
+          "Pitting potential": joblib.load(f"{model_dir}/RF_Pcorr")
+          }
 
 
 
