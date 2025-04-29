6b495f5e8f08b24dbbbda8fc29fa7e8c2dc36248
Marzie Ghorbani <60660630+katrina-coder@users.noreply.github.com>
2025-04-29 11:42:33 +1000

    Add files via upload

---
+++ model_paths.py
@@ -0,0 +1,24 @@
+import joblib
+import pickle
+import warnings
+
+
+
+
+if 'google.colab' in str(get_ipython()):
+    model_dir = "MPEAsAlloyPublic/models"
+else:
+    model_dir = "models"
+    
+# with warnings.catch_warnings():
+#     warnings.simplefilter("ignore", category=UserWarning)
+
+warnings.filterwarnings('ignore')
+
+models = {"Current density": joblib.load(f"{model_dir}/RF_icorr"),
+          "Corrosion potential": joblib.load(f"{model_dir}/RF_Ecorr"),
+          "Pitting potential": joblib.load(f"{model_dir}/RF_Pcorr")
+          }
+
+
+
